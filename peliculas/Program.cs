eaff0ba31d7dabba39263490c17e04364298a9cc
Pablo Alberto Espinoza Ruiz <pabloalber84@gmail.com>
2019-09-09 21:48:25 -0700

    [13/09/2019] V2

--- peliculas/Program.cs
+++ peliculas/Program.cs
@@ -4,28 +4,31 @@ namespace peliculas
 {
 	class Pelicula
 	{
-		public string titulo;
-		public Int16 año;
-		public string pais;
-		public string director;
+		private string titulo;
+		private Int16 año;
+		private string pais;
+		private string director;
+		public void SetTitulo(string titulo) {
+			this.titulo = titulo;
+		}
+		public string GetTitulo() {
+			return this.titulo;
+		}
+		public void SetAño(Int16 año) {
+			this.año = año;
+		}
+		public Int16 GetAño() {
+			return this.año;
+		}
 	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			Pelicula p1 = new Pelicula();
-			p1.titulo = "La forma del agua";
-			p1.año = 2017;
-			p1.pais = "Estados Unidos";
-			p1.director = "Guillermo del Toro";
-
-			Pelicula p2 = new Pelicula();
-			p2.titulo = "Birdman";
-			p2.año = 2014;
-			p2.pais = "Estados Unidos";
-			p2.director = "Alejandro González Iñárritu";
-
-			Console.WriteLine("Peliculas nominadas al Oscar:\n{0} - {1}\n{2} - {3}", p1.titulo, p1.año, p2.titulo, p2.año);
+			p1.SetTitulo("La La Land");
+			p1.SetAño(2016); 
+			Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
 		}
 	}
 }
