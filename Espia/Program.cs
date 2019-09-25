1fbd3430ab74f07b68f5351b82a0d9edc97fb609
rwb3x <54416678+rwb3x@users.noreply.github.com>
2019-09-25 12:10:03 -0700

    Version 1

---
+++ Espia/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Espia
+{
+
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+        public Persona()
+        {
+            nombre = "Pablo";
+            apellido = "Espinoza";
+        }
+
+    }
+
+    class Anonimo 
+    {
+        public static void anonimiza(Persona p)
+        {
+            p.nombre = "xxxxx";
+            p.apellido = "xxxxxxxxxx";
+        }
+
+        public static void cambia(ref  Persona p)
+        {
+            p = new Persona();
+            p.nombre = "new";
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            Console.WriteLine(p.nombre);
+            //Anonimo.anonimiza(p);
+            Anonimo.cambia(ref p);
+            Console.WriteLine(p.nombre);
+        }
+    }
+}
